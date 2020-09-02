f71cc2e8174a79ad5338c2c71e89b59d66914aa7
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2020-09-02 09:22:58 +0530

    Add files via upload

--- arrays.h
+++ arrays.h
@@ -59,19 +59,106 @@ int count_primes(int *arr,int size)//3 4 6 7 2
 }
 
 
+int even_count(int *arr,int size)
+{
+	int i,ec=0;
+	for(i=0;i<size;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			ec++;
+		}
+	}
+	return ec;
+}
+
+int odd_count(int *arr,int size)
+{
+	int i,oc=0;
+	for(i=0;i<size;i++)
+	{
+		if(arr[i]%2==1)
+		{
+			oc++;
+		}
+	}
+	return oc;
+}
 
 
+void even_odd_count(int *arr,int size,int *ec,int *oc)
+{
+	int i;
+	for(i=0;i<size;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			*ec=*ec+1;
+		}
+		else
+		{
+			*oc=*oc+1;
+		}
+	}
+}
 
 
+void evenodd_count(int *arr,int size,int *eoc)
+{
+	int i;
+	for(i=0;i<size;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			eoc[0]=eoc[0]+1;
+		}
+		else
+		{
+			eoc[1]=eoc[1]+1;
+		}
+	}
+}
 
 
+void evenodd_own_count(int *arr,int *size)
+{
+	int i,ec=0,oc=0;
+	for(i=0;i<*size;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			ec++;
+		}
+		else
+		{
+			oc++;
+		}
+	}
+	arr[0]=ec;
+	arr[1]=oc;
+	*size=2;
+}
 
-
-
-
-
-
-
+//int eoc[2]={0,0};
+int * evenodd_return_count(int *arr,int *size)
+{
+	int i;
+	static int eoc[2]={0,0};
+	for(i=0;i<*size;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			eoc[0]++;
+		}
+		else
+		{
+			eoc[1]++;
+		}
+	}
+	*size=2;
+	//printf("%d",eoc);
+	return eoc;//
+}
 
 
 
