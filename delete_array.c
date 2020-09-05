c8a0d7822c69f6d2305d3868c18fabe34b212fae
sudhir33 <32663121+sudhir33@users.noreply.github.com>
2020-09-05 21:01:03 +0530

    Add files via upload

--- delete_array.c
+++ delete_array.c
@@ -1,3 +1,19 @@
+#include<stdio.h>
+//5
+//10 20 40 50 50
+//3
+void delete_array(int *arr,int *size,int pos)
+{
+	int i;
+	for(i=pos-1;i<*size-1;i++)
+	{
+		//printf("%d ",arr[i]);
+		arr[i]=arr[i+1];
+		
+	}
+	//printf("%d\n",arr[pos-1]);
+	*size=*size-1;
+}
 int main()
 {
 	int i,size,arr[100],pos;
@@ -7,11 +23,11 @@ int main()
 		scanf("%d",&arr[i]);
 	}
 	scanf("%d",&pos);
-	delete_array(arr,&size,pos);//
+	delete_array(arr,&size,pos);//fun call
 	for(i=0;i<size;i++)
 	{
 		printf("%d ",arr[i]);
-	}
+	}//
 	return 0;
 }
 
